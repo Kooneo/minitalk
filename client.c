ccf53b8001d7d535393e624bcdaf9f97ece600c8
Kooneo <kooneo@Kooneos-MacBook-Pro.local>
2024-12-28 18:46:20 +0100

    wip

--- client.c
+++ client.c
@@ -6,16 +6,75 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 16:20:25 by zbakour           #+#    #+#             */
-/*   Updated: 2024/12/24 21:50:09 by zbakour          ###   ########.fr       */
+/*   Updated: 2024/12/28 18:45:53 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
+// void	print_bits(int *bits)
+// {
+// 	for (int j = 8 - 1; j >= 0; j--)
+// 		ft_printf("%d", bits[j]);
+// }
+
+void	send_char(int *cab, int pid)
+{
+	int	i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		if (cab[i])
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		ft_printf("%d", cab[i]);
+		i--;
+	}
+}
+
+void	ascii_to_bit(char c, int pid)
+{
+	int	bits[8];
+	int	i;
+
+	i = 0;
+	while (i < 8)
+		bits[i++] = 0;
+	i = 0;
+	while (c > 0)
+	{
+		bits[i] = c % 2;
+		c = c / 2;
+		i++;
+	}
+	send_char(bits, pid);
+}
+
+void	message_encryption(char *msg, int pid)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(msg))
+	{
+		ascii_to_bit(msg[i], pid);
+		i++;
+	}
+}
+
 int main(int argc, char **argv)
 {
-    if (argc == 3)
-    {
-        ft_printf("%s", argv[1]);
-    }
+	int     pid;
+	char    *message;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		message = argv[2];
+		if (pid < 0 || !message || message[0] == '\0')
+			return (1);
+		message_encryption(message, pid);
+	}
 }
--- main.c
+++ main.c
@@ -1,18 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.h                                           :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/23 16:21:33 by zbakour           #+#    #+#             */
-/*   Updated: 2024/12/28 18:41:32 by zbakour          ###   ########.fr       */
+/*   Created: 2024/12/28 18:41:55 by zbakour           #+#    #+#             */
+/*   Updated: 2024/12/28 18:41:57 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERVER_H
-# define SERVER_H
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-# include "libft/libft.h"
+void	signal_handler(int signum)
+{
+	printf("Received signal %d\n", signum);
+}
 
-#endif
+int	main(void)
+{
+	struct sigaction action;
+	action.sa_handler = signal_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+
+	sigaction(SIGINT, &action, NULL);
+
+	while (1)
+	{
+		// Do some work
+	}
+
+	return (0);
+}
